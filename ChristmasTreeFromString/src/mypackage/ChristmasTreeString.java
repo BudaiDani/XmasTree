44229824576bbc2fe5a0ad8bb8ae3971fd733648
Budai Dani <budai.d945@gmail.com>
2016-10-06 11:53:40 +0200

    refactor CTS class

--- ChristmasTreeFromString/src/mypackage/ChristmasTreeString.java
+++ ChristmasTreeFromString/src/mypackage/ChristmasTreeString.java
@@ -3,19 +3,20 @@ package mypackage;
 public class ChristmasTreeString {
 
 	public static void main(String[] args) {
-		makeTriangle("Hello World!");
+		makeTriangle("Hello");
 	}
 
 	public static void makeTriangle(String input) {
 		for (int i = 0; i < input.length(); i++) {
-			for (int k = 0; k < input.length() - i; k++) {
+
+			for (int j = 0; j < input.length() - i; j++) {
 				System.out.print(" ");
 			}
 
-			for (int j = 0; j <= i; j++) {
+			for (int k = 0; k <= 2 * i; k++) {
 				char c;
 				c = input.charAt(i);
-				System.out.print(c + " ");
+				System.out.print(c);
 			}
 			System.out.println(" ");
 		}
